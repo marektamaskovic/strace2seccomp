59153b3882ef3a05875498a0eef9c80e90307080
Marek Tamaskovic <tamaskovic.marek@gmail.com>
2018-04-26 12:49:56 +0200

    add class diagram and some other text

---
+++ obrazky-figures/class/svg2pdf.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+EXEC='algo'
+HIGH='arg_sc'
+LOW='params'
+OPTI='optimizer'
+
+rsvg-convert -f pdf "$EXEC.svg" -o "$EXEC.pdf"
+rsvg-convert -f pdf "$HIGH.svg" -o "$HIGH.pdf"
+rsvg-convert -f pdf "$LOW.svg"  -o "$LOW.pdf"
+rsvg-convert -f pdf "$OPTI.svg" -o "$OPTI.pdf"
+
+#ps2pdf -i "$EXEC.pdf" -o "$EXEC.pdf"
+#ps2pdf -i "$HIGH.pdf" -o "$HIGH.pdf"
+#ps2pdf -i "$LOW.pdf" -o "$LOW.pdf"
