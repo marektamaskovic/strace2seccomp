90630587765c4b26c67a338237a6af82f0bb94f0
Marek Tamaskovic <mtamasko@redhat.com>
2018-07-14 00:20:24 +0200

    Add GPLv3 licence into files

--- src/states.cpp
+++ src/states.cpp
@@ -1,3 +1,21 @@
+// Copyright (c) 2018 Red Hat <mtamasko@redhat.com>
+// Author: Marek Tamaskovic <tamaskovic.marek@gmail.com>
+//
+// This file is part of strace2seccomp tool.
+//
+// strace2seccomp tool is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// strace2seccomp tool is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with strace2seccomp tool.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "states.hpp"
 
 template<class T> struct always_false : std::false_type {};
